00f9e2bf0c16bdedabba6afe2b8d500ca6be3db5
Md Mehedi Khan Maruf <mkmaruf1616@gmail.com>
2026-09-01 21:38:28 +0600

    Update protected pages for admin access
    Removed 'Register Student' from protected pages list.

--- app.py
+++ app.py
@@ -75,7 +75,7 @@ menu = st.sidebar.selectbox(
 # -------------------------------------------------------------
 # PASSWORD PROTECTION CHECK FOR ADMIN PAGES
 # -------------------------------------------------------------
-protected_pages = ["Mark Attendance", "Register Student", "Manage Students"]
+protected_pages = ["Mark Attendance",  "Manage Students"]
 
 if menu in protected_pages:
     if "authenticated" not in st.session_state:
